79166b0b646eb19eea8dfa5abe2558df02dd8dda
Zhanna Popenko <zpopenko7@gmail.com>
2021-09-02 00:29:37 +0300

    Implemented "Save" and "Change" buttons

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import style from "./Todo.module.css";
 
 export const Todo = ({ todo, toggleTodo, removeTodo }) => {
+  const [changeItem, setChangeItem] = useState(false);
+  const [updateInputValue, setUpdateInputValue] = useState(todo.task);
+
   return (
     <div key={todo.id} className={style.todoInput}>
       <div
         className={todo.complete && style.todoComplete}
-        onClick={() => toggleTodo(todo.id)}
-      >
-        {todo.task}
+        onClick={() => toggleTodo(todo.id)} >
+        
+        {changeItem ? 
+        (<input value={updateInputValue} onChange={(e) => setUpdateInputValue(e.target.value)}/>) 
+        : (updateInputValue)
+        }
       </div>
-      <div onClick={() => removeTodo(todo.id)}>Remove</div>
+
+        {changeItem ?
+        (<button onClick={() => {setChangeItem(false);}}>Save</button> ) 
+        : (<button onClick={() => {setChangeItem(true);}}>Change</button>)
+        }
+
+      <button onClick={() => removeTodo(todo.id)}>Remove</button>
     </div>
   );
 };
